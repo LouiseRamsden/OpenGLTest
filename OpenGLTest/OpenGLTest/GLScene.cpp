ea15f9ca086a75e0892bb8bea74a718da7764577
r018419j <louiseramsden2@gmail.com>
2024-03-14 09:02:30 +0000

    fgh

--- OpenGLTest/OpenGLTest/GLScene.cpp
+++ OpenGLTest/OpenGLTest/GLScene.cpp
@@ -5,7 +5,7 @@ GLScene::GLScene(int argc, char* argv[])
 	GLUTCallbacks::Init(this);
 	glutInit(&argc, argv);
 	glutInitWindowSize(800, 800);
-	glutCreateWindow("Hello World");
+	glutCreateWindow("Hello World!");
 	glutDisplayFunc(GLUTCallbacks::Display);
 	glutMainLoop();
 }
